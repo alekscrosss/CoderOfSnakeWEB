513b58bba8bd8d7cbb88a7319155d7b9675f7776
OlhaVerbova <137227019+OlhaVerbova@users.noreply.github.com>
2024-02-18 21:10:54 +0200

    Нова папка Config
    в ній файл з messages.py
    у файлі зручно зберігати стандартні повідомлення на всі випадки, також юзати їх у тестах.

---
+++ conf/messages.py
@@ -0,0 +1,6 @@
+EMAIL_NOT_CONFIRMED = "Email not confirmed!"
+ACCOUNT_ALREADY_EXISTS = "Account already exists!"
+INVALID_PASSWORD = "Invalid password!"
+COULD_NOT_VALIDATE_CREDATIALS="Could not validate credentials"
+INVALID_SCOPE_FOR_TOKEN='Invalid scope for token'
+INVALID_TOKEN_FOR_EMAIL_VARIFICATION='Invalid token for email verification'

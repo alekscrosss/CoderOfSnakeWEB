ffcc76b9d89d3137fb9e39d47e491448a0ad18a9
Oiseauoui <oiseua@gmail.com>
2024-02-18 14:44:38 +0200

    Photo

--- crud.py
+++ crud.py
@@ -34,3 +34,33 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 def verify_password(plain_password, hashed_password):
     # Проверка на правильность пароля
     return pwd_context.verify(plain_password, hashed_password)
+
+
+# Iuliia 18.02.24
+def create_photo(db: Session, filename: str):
+    # Створити новий запис про фотографію в базі даних
+    photo = models.Photo(filename=filename)
+    db.add(photo)
+    db.commit()
+    db.refresh(photo)
+    return photo
+
+
+# Iuliia 18.02.24
+def delete_photo(db: Session, photo_id: int):
+    # Видалення фотографії з бази даних за її ідентифікатором
+    db.query(models.Photo).filter(models.Photo.id == photo_id).delete()
+    db.commit()
+
+
+# Iuliia 18.02.24
+def update_photo(db: Session, photo_id: int, photo_data: dict):
+    # Оновлення інформації про фотографію в базі даних
+    db.query(models.Photo).filter(models.Photo.id == photo_id).update(photo_data)
+    db.commit()
+
+
+# Iuliia 18.02.24
+def get_photo(db: Session, photo_id: int):
+    # Отримання фотографії з бази даних за її ідентифікатором
+    return db.query(models.Photo).filter(models.Photo.id == photo_id).first()

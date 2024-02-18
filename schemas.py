2804e130b665a45296430d9275c20e1d7b809ec6
OlhaVerbova <137227019+OlhaVerbova@users.noreply.github.com>
2024-02-18 22:01:00 +0200

    Schemas - добавлены функции
    Schemas - добавлены функции
    class UserModel(BaseModel):
    class RequestEmail(BaseModel):
    class TokenModel(BaseModel):

--- schemas.py
+++ schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field #18/02/2024 Olha try4
 from typing import Optional
 
 
@@ -9,6 +9,10 @@ class UserBase(BaseModel):
 class UserCreate(UserBase):
     password: str
 
+class UserModel(BaseModel): #18/02/2024 Olha try4
+    username: str = Field(min_length=2, max_length=50)
+    password: str = Field(min_length=6, max_length=50)
+    email: EmailStr
 
 class User(UserBase):
     id: int
@@ -20,6 +24,14 @@ class User(UserBase):
 
 class TokenData(BaseModel):
     username: Optional[str] = None
+
+class TokenModel(BaseModel): #18/02/2024 Olha try4
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+    
+class RequestEmail(BaseModel): #18/02/2024 Olha try4
+    email: EmailStr
 
 
 # Iuliia 18.02.24 -  Photo

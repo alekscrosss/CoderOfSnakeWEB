d84a26c04cba9b1f75b6e95d5f4f47d8be4e7861
OlhaVerbova <137227019+OlhaVerbova@users.noreply.github.com>
2024-02-18 21:55:52 +0200

    messages - добавлены значения
    messages - добавлены значения
    
    INVALID_REFRESH_TOKEN="Invalid refresh token" #new
    VERIFICATION_ERROR="Verification error

--- conf/messages.py
+++ conf/messages.py
@@ -4,3 +4,5 @@ INVALID_PASSWORD = "Invalid password!"
 COULD_NOT_VALIDATE_CREDATIALS="Could not validate credentials"
 INVALID_SCOPE_FOR_TOKEN='Invalid scope for token'
 INVALID_TOKEN_FOR_EMAIL_VARIFICATION='Invalid token for email verification'
+INVALID_REFRESH_TOKEN="Invalid refresh token" #new
+VERIFICATION_ERROR="Verification error"

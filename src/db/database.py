5f253b271c02325542098d2f8eb603e6c380a702
OlhaVerbova <137227019+OlhaVerbova@users.noreply.github.com>
2024-02-28 11:59:26 +0200

    документация к функции 7

--- src/db/database.py
+++ src/db/database.py
@@ -3,6 +3,14 @@ from src.db import models
 
 
 def get_db():
+    
+    """
+    The get_db function opens a new database connection if there is none yet for the current application context.
+    It will also create the database tables if they don't exist yet.
+    
+    :return: A database connection
+    :doc-author: Trelent
+    """
     db = models.SessionLocal()
     try:
         yield db
